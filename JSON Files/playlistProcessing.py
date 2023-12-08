dd56d054117421cac0debe0626c7480d87cf8edc
Valentina <villarroelvalentina@MacBook-Air-6.local>
2023-12-07 21:45:50 -0500

    Changed code for JSON

--- JSON Files/playlistProcessing.py
+++ JSON Files/playlistProcessing.py
@@ -1,17 +1,16 @@
 import json
+import pandas as pd
 
 playlist_data =[]
-playlist_files = ['JSON Files/Playlists/best70s80s.json','JSON Files/Playlists/classic_oldies_mix1.json', 'JSON Files/Playlists/classic_oldies2.json', 'JSON Files/Playlists/the80s_party.json', 'JSON Files/Playlists/the90s_hits.json', 'JSON Files/Playlists/the1950s.json', 'JSON Files/Playlists/the1960s.json', 'JSON Files/Playlists/the1970s.json', 'JSON Files/Playlists/the1980s.json', 'JSON Files/Playlists/the1990s.json', 'JSON Files/Playlists/the2000s.json']
+playlist_files = ['JSON Files/Playlists/the1990s.json', 'JSON Files/Playlists/the50s_1.json', 'JSON Files/Playlists/the50s_2.json', 'JSON Files/Playlists/the50s_3.json', 'JSON Files/Playlists/the50s_5.json', 'JSON Files/Playlists/the50s_6.json', 'JSON Files/Playlists/the50s_8.json', 'JSON Files/Playlists/the50s_9.json', 'JSON Files/Playlists/the90s_1.json', 'JSON Files/Playlists/the90s_2.json', 'JSON Files/Playlists/the90s_3.json', 'JSON Files/Playlists/the00s_1.json', 'JSON Files/Playlists/the00s_2.json', 'JSON Files/Playlists/the00s_3.json', 'JSON Files/Playlists/the00s_4.json', 'JSON Files/Playlists/the00s_5.json', 'JSON Files/Playlists/1960s_deep_cuts.json', 'JSON Files/Playlists/1960s_underground.json', 'JSON Files/Playlists/the70s_hits_2.json', 'JSON Files/Playlists/the70s_hits_3.json', 'JSON Files/Playlists/1980s_hits_part_1.json', 'JSON Files/Playlists/1980s_hits_part_2.json', 'JSON Files/Playlists/best70s80s.json','JSON Files/Playlists/classic_oldies_mix1.json', 'JSON Files/Playlists/classic_oldies2.json', 'JSON Files/Playlists/the80s_party.json', 'JSON Files/Playlists/the90s_hits.json', 'JSON Files/Playlists/the1950s.json', 'JSON Files/Playlists/the1960s.json', 'JSON Files/Playlists/the1970s.json', 'JSON Files/Playlists/the1980s.json', 'JSON Files/Playlists/the2000s.json']
 full_str = ''
-full_str_50 = ''
-j = 0
 for i in range(len(playlist_files)):
     full_str += '\n' + playlist_files[i] + '\n'
-    full_str_50 += '\n' + playlist_files[i] + '\n'
+    print(playlist_files[i])
     curr_file = open(playlist_files[i])
     curr_data = json.load(curr_file)
+    print(len(curr_data))
     id_str = ''
-    j = 0
     for item in curr_data['tracks']['items']:
         song_dict = {}
         curr_track = item['track']
@@ -25,19 +24,12 @@ for i in range(len(playlist_files)):
             song_dict['release_year']  = curr_track['album']['release_date'].split('-')[0]
         else:
             song_dict['release_year'] = curr_track['album']['release_date']
-        if j == 50:
-            full_str_50 += '\n'
         full_str = full_str + id_str
-        full_str_50 = full_str_50 + id_str
         playlist_data.append(song_dict)
-        j += 1
     full_str += '\n'
-    full_str_50 += '\n'
 
 with open("playlist_dict.json", "w") as outfile:
     json.dump(playlist_data, outfile)
 
 with open('curl_ids.txt', 'w') as f:
     f.write(full_str)
-    f.write('________________________________________________________________\n')
-    f.write(full_str_50)
